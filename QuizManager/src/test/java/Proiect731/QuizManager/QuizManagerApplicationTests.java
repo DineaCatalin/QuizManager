1bb006dd98934fb7c2e4fdd681d94d7a62944a9c
Radu Falcusan <falcusanradudan@gmail.com>
2017-12-09 16:00:14 +0200

    @Ignore added to tests

--- QuizManager/src/test/java/Proiect731/QuizManager/QuizManagerApplicationTests.java
+++ QuizManager/src/test/java/Proiect731/QuizManager/QuizManagerApplicationTests.java
@@ -1,9 +1,11 @@
 package Proiect731.QuizManager;
 
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@Ignore
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class QuizManagerApplicationTests {
